ac6d3481a0ae97fc4945367d15a0dff9641ca003
muhammadilham30 <ilhamwadini@student.uns.ac.id>
2023-03-13 17:37:13 +0700

    menambahkan file identitas

---
+++ Modul_01/Exercise-01.py
@@ -0,0 +1,13 @@
+Nama = "Muhammad Ilham Wadini"
+Nim = "I0322084"
+Kota_Asal = "Bukittinggi"
+Deskripsi_Kampunghalaman =  "Bukittinggi merupakan suatu kota yang terletak di daerah perbukitan, wajar saja kalau kota ini bersuhu dingin. BUkittingi merupakan kota kecil dengan penduduk yang padat, mayoritas penduduknya berprofesi sebagai pedagang karena merupakan kota wisata. Di Bukittinggi terdapat objek eisata terkenal seperti Jam Gadang dan Lobang Jepang."
+Harapan = "setelah lulus dari Teknik Industri UNS adalah memiliki pengalaman dan ilmu baru, serta cepat dapat mendapatkan pekerjaan sehinnga dapat membantu orang-orang di sekitar saya."
+
+print("========Assalamualikum wR.WB========")
+print("Perkenalkan nama saya ", Nama)
+print("Nim saya ", Nim)
+print("Saya berasal dari Kota ", Kota_Asal)
+print("Kota ", Deskripsi_Kampunghalaman)
+print("Harapan saya ", Harapan)
+print("========Sekian Terima Kasih========")
